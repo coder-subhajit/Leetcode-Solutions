acc6a2558fc5596c32635da7d690b70188886194
CODER__SUBHAJIT <subhajitbarik909@gmail.com>
2026-07-21 21:28:35 +0530

    Time: 2 ms (99.29%), Space: 44 MB (92.11%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,28 +1,20 @@
 class Solution {
     public boolean isPalindrome(String s) {
-
-        int left = 0;
-        int right = s.length() - 1;
-
-        while (left < right) {
-
-            while (left < right && !Character.isLetterOrDigit(s.charAt(left))) {
+        int left=0;
+        int right=s.length()-1;
+        while(left<right){
+            while(left<right&& !Character.isLetterOrDigit(s.charAt(left))){
                 left++;
             }
-
-            while (left < right && !Character.isLetterOrDigit(s.charAt(right))) {
+            while(left<right&& !Character.isLetterOrDigit(s.charAt(right))){
                 right--;
             }
-
-            if (Character.toLowerCase(s.charAt(left)) !=
-                Character.toLowerCase(s.charAt(right))) {
+            if(Character.toLowerCase(s.charAt(left))!=Character.toLowerCase(s.charAt(right))){
                 return false;
             }
-
-            left++;
-            right--;
+                left++;
+                right--;
         }
-
         return true;
     }
 }
