39543b00dad749c58aa8da3e9c677d5cb84fa82c
CODER__SUBHAJIT <subhajitbarik909@gmail.com>
2026-07-24 00:47:19 +0530

    Time: 2 ms (2.73%), Space: 46.6 MB (41.37%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.java
+++ 1470-shuffle-the-array/1470-shuffle-the-array.java
@@ -1,9 +1,14 @@
+import java.util.*;
 class Solution {
     public int[] shuffle(int[] nums, int n) {
-     int[] result=new int[2*n] ;
-     for(int i=0;i<n;i++){
-        result[2*i]=nums[i];
-        result[2*i+1]=nums[i+n];
-     } return result;
+    ArrayList<Integer>list=new ArrayList<>();
+    for(int i=0;i<n;i++){
+        list.add(nums[i]);//xi
+        list.add(nums[i+n]);//yi
     }
+        int[] ans=new int[2*n];
+        for(int i=0;i<ans.length;i++){
+            ans[i]=list.get(i);
+  }return ans;
+}
 }
