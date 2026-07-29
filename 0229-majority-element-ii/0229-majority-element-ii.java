88f106aca9d29b1da87dde4831462e14f35129d2
CODER__SUBHAJIT <subhajitbarik909@gmail.com>
2026-07-29 21:17:20 +0530

    Time: 2 ms (99.95%), Space: 52.4 MB (66.34%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,14 +1,43 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-     HashMap<Integer,Integer>map=new HashMap<>();
+     int element1=0;
+     int element2=0;
+     int count1=0;
+     int count2=0;
+     //find two majority element
      for(int num : nums){
-        map.put(num,map.getOrDefault(num,0)+1);
-     }
-     ArrayList<Integer>ans=new ArrayList<>();
-     for(int key : map.keySet()){
-        if(map.get(key)>nums.length/3){
-            ans.add(key);
+        if(num==element1){
+            count1++;
+        }else if(num==element2){
+            count2++;
+        }else if(count1==0&&num!=element2){
+            element1=num;
+            count1=1;
+        }else if(count2==0&&num!=element1){
+            element2=num;
+            count2=1;
         }
-     }return ans;
+        else{
+            count1--;
+            count2--;
+        }
+     }
+        //check the conditions
+        count1=0;
+        count2=0;
+        for(int num : nums){
+            if(num==element1){
+                count1++;
+            }else if(num==element2){
+                count2++;
+            }  
+        }
+       ArrayList<Integer>ans=new ArrayList<>();
+       if(count1>nums.length/3){
+        ans.add(element1);
+       }
+       if(count2>nums.length/3){
+        ans.add(element2);
+       } return ans;
     }
 }
