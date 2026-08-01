7efc30e7f08c44abff642a79db07d27a875d6e7a
CODER__SUBHAJIT <subhajitbarik909@gmail.com>
2026-08-01 21:37:09 +0530

    Time: 0 ms (100%), Space: 43.3 MB (78.27%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,33 +1,3 @@
-//optimized but not optimal.For optimal solution use DNF(Dutch National Flag) algo...
-class Solution {
-    public void sortColors(int[] nums) {
-     int n=nums.length;
-     int count_0=0;
-     int count_1=0;
-     int count_2=0;
-     for(int i=0;i<n;i++){
-        if(nums[i]==0){
-            count_0++;
-        }else if(nums[i]==1){
-            count_1++;
-        }else{
-            count_2++;
-        }  
-     }
-     int index=0;
-     for(int i=0;i<count_0;i++){
-        nums[index++]=0;
-     }   
-     for(int i=0;i<count_1;i++){
-        nums[index++]=1;
-     }
-     for(int i=0;i<count_2;i++){
-        nums[index++]=2;
-    } 
- } 
-}
-
-//optimal 
 class Solution {
     public void sortColors(int[] nums) {
      int low=0;
