fe104f64081518edb9f37782d94468d3ce850724
CODER__SUBHAJIT <subhajitbarik909@gmail.com>
2026-08-15 20:23:46 +0530

    Time: 29 ms (93.36%), Space: 59.2 MB (60.23%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,27 +1,62 @@
 import java.util.*;
+
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        if(nums==null||nums.length<3){
-            return new ArrayList<>();
-        } 
+
+        List<List<Integer>> result = new ArrayList<>();
+
         Arrays.sort(nums);
-        Set<List<Integer>>Result=new HashSet<>();
-        for(int i=0;i<nums.length-2;i++){
-            int left=i+1;
-            int right=nums.length-1;
-            while(left<right){
-                int sum=nums[i]+nums[left]+nums[right];
-                if(sum==0){
-                    Result.add(Arrays.asList(nums[i],nums[left],nums[right]));
+
+        for (int i = 0; i < nums.length - 2; i++) {
+
+            // Skip duplicate i
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+
+            // Optimization
+            if (nums[i] > 0) {
+                break;
+            }
+
+            int left = i + 1;
+            int right = nums.length - 1;
+
+            while (left < right) {
+
+                int sum = nums[i] + nums[left] + nums[right];
+
+                if (sum == 0) {
+
+                    result.add(Arrays.asList(
+                        nums[i],
+                        nums[left],
+                        nums[right]
+                    ));
+
                     left++;
                     right--;
-                }else if(sum<0){
+
+                    // Skip duplicate left values
+                    while (left < right &&
+                           nums[left] == nums[left - 1]) {
+                        left++;
+                    }
+
+                    // Skip duplicate right values
+                    while (left < right &&
+                           nums[right] == nums[right + 1]) {
+                        right--;
+                    }
+
+                } else if (sum < 0) {
                     left++;
-                }else{
+                } else {
                     right--;
                 }
             }
-        } 
-        return new ArrayList<>(Result);  
+        }
+
+        return result;
     }
 }
