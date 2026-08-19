721a26af8583bacc9781b654f9c0beeabd680240
CODER__SUBHAJIT <subhajitbarik909@gmail.com>
2026-08-19 21:04:06 +0530

    Time: 2 ms (70.94%), Space: 55.5 MB (66.78%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,26 +1,27 @@
-//Moore's Voting Algo...
 class Solution {
     public int majorityElement(int[] nums) {
-        int count=0;
-        int element=0;
-        for(int num : nums){
-            if(count==0){
-                element=num;
-            }
-            if(num==element){
-                count++;
-            }else{
-                count--;
-            }
+       int elm=0;
+       int count=0;
+       for(int num : nums){
+        if(count==0){
+            elm=num;
         }
-        count=0;
-        for(int num : nums){
-            if(num==element){
-                count++;
-            } 
-            if(count>nums.length/2){
-                return element;
-            }
-        } return -1;
+        if(num==elm){
+            count++;
+        }else{
+        count--;
+       }
+       }
+       count=0;
+       for(int num : nums){
+        if(num==elm){
+            count++;
+        }
+       }
+        if(count>nums.length/2){
+            return elm;
+        }else{
+            return -1;
+        }
     }
 }
