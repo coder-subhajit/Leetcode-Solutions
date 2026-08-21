ec006d8e27f925286630650995797f4c096f6a7d
CODER__SUBHAJIT <subhajitbarik909@gmail.com>
2026-08-21 20:16:25 +0530

    Time: 4 ms (44.97%), Space: 50.4 MB (5.15%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,20 +1,24 @@
 class Solution {
-    public boolean isPalindrome(String s) {
-        int left=0;
-        int right=s.length()-1;
-        while(left<right){
-            while(left<right&& !Character.isLetterOrDigit(s.charAt(left))){
-                left++;
-            }
-            while(left<right&& !Character.isLetterOrDigit(s.charAt(right))){
-                right--;
-            }
-            if(Character.toLowerCase(s.charAt(left))!=Character.toLowerCase(s.charAt(right))){
-                return false;
-            }
-                left++;
-                right--;
+    static boolean pal(String s,int left,int right){
+        //skip non-aplphaneumeric characters
+        while(left<right&&!Character.isLetterOrDigit(s.charAt(left))){
+        left++;
         }
-        return true;
+         while(left<right&&!Character.isLetterOrDigit(s.charAt(right))){
+        right--;
+        }
+        //base case
+        if(left>=right){
+            return true;
+        }
+        //character's does not match
+        if(Character.toLowerCase(s.charAt(left))!=Character.toLowerCase(s.charAt(right))){
+            return false;
+        }
+        //recursive call
+        return pal(s,left+1,right-1);
+    }
+    public boolean isPalindrome(String s) {
+     return pal(s,0,s.length()-1);   
     }
 }
