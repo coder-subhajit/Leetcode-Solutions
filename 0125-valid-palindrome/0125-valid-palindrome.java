7ab1e5c1d2126e91c3b77ba12d6a5c5316ed4658
CODER__SUBHAJIT <subhajitbarik909@gmail.com>
2026-09-02 00:13:13 +0530

    Time: 6 ms (34.78%), Space: 50.9 MB (5.09%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,24 +1,21 @@
 class Solution {
     static boolean pal(String s,int left,int right){
-        //skip non-aplphaneumeric characters
         while(left<right&&!Character.isLetterOrDigit(s.charAt(left))){
-        left++;
+            left++;
         }
-         while(left<right&&!Character.isLetterOrDigit(s.charAt(right))){
-        right--;
+        while(left<right&&!Character.isLetterOrDigit(s.charAt(right))){
+            right--;
         }
         //base case
         if(left>=right){
             return true;
         }
-        //character's does not match
-        if(Character.toLowerCase(s.charAt(left))!=Character.toLowerCase(s.charAt(right))){
-            return false;
-        }
-        //recursive call
-        return pal(s,left+1,right-1);
+    if(Character.toLowerCase(s.charAt(left))!=Character.toLowerCase(s.charAt(right))){
+        return false;
+      }
+       return pal(s,left+1,right-1);
     }
-    public boolean isPalindrome(String s) {
-     return pal(s,0,s.length()-1);   
+    public boolean isPalindrome(String s){
+        return pal(s,0,s.length()-1);
     }
 }
